324c30bd8edb47eba875da18362c001b9407470f
Solmex <solmexgaming@gmail.com>
2018-02-04 15:26:19 -0500

    Fixed something for Keough

--- src/main/java/com/maximuspayne/navycraft/MoveCraft_PlayerListener.java
+++ src/main/java/com/maximuspayne/navycraft/MoveCraft_PlayerListener.java
@@ -3670,7 +3670,8 @@ public class MoveCraft_PlayerListener implements Listener {
 								+ ChatColor.GRAY + " rank points.");
 					} else {
 						player.sendMessage("That command does not work in this world");
-					} else {
+					} 
+				}else {
 					player.sendMessage("Group manager error");
 					return;
 				}
